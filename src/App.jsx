7a7ab13d43915abfd62fadbe833d759049115a56
Mohammed <sajidcurious9@gmail.com>
2023-10-18 21:36:33 +0530

    building Shazam Search Engine

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import SearchResult from "./components/SearchResult";
+import { AppContext } from "./utils/ContextApi";
+
+function App() {
+    return <div>Google search app || JS Dev</div>;
+}
+
+export default App;
