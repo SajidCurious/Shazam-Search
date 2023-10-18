fc43ae2a88ffffcf611357ba808e877e4eb5b3d7
Mohammed <sajidcurious9@gmail.com>
2023-10-18 23:09:47 +0530

    added search box

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -7,7 +7,23 @@ import MicIcon from "../assets/mic.svg";
 import ImageIcon from "../assets/image.svg";
 
 const SearchInput = () => {
-    return <div>Search input</div>;
+  return (
+    <div
+      id="searchBox"
+      className="h-[46px] w-full md:w-[584px] flex items-center gap-3 px-4 border border-[#dfe1e5] rounded-3xl hover:bg-white hover:shadow-c hover:border-0 focus-within:shadow-c focus-within:border-0"
+    >
+      <AiOutlineSearch size={18} color="#9aa0a6" />
+      <input
+        type="text"
+        autoFocus
+        className="grow outline-0 text-black/[0.87]"
+      />
+      <div className="flex items-center gap-3">
+        <img className="h-6 w-6 cursor-pointer" src={MicIcon} alt="" />
+        <img className="h-6 w-6 cursor-pointer" src={ImageIcon} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default SearchInput;
