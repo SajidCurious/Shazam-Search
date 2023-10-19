c972e8a0f07ed6ecd2dd46df71f79f83fbd09aeb
Mohammed <sajidcurious9@gmail.com>
2023-10-19 22:19:20 +0530

    edited

--- src/components/SearchResultHeader.jsx
+++ src/components/SearchResultHeader.jsx
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
 import { useContext, useState, useEffect } from "react";
+import { GoSearch } from "react-icons/go";
+import { BsImage } from "react-icons/bs";
+import { BiNews } from "react-icons/bi";
+import { RiVideoLine } from "react-icons/ri";
+import { SlTag } from "react-icons/sl";
 
-import Logo from "../assets/s-logos_transparent.png";
+import Logo from "../assets/google-logo.png";
 import SearchInput from "./SearchInput";
 import ProfileIcon from "./ProfileIcon";
 import { Context } from "../utils/ContextApi";
 import { menu } from "../utils/Constants";
 
 const SearchResultHeader = () => {
-  const clickHandler = () => {};
+  const [selectedMenu, setSelectedMenu] = useState("All");
+
+  const clickHandler = (menuItem) => {
+    setSelectedMenu(menuItem.name);
+  };
+
   return (
     <div className="p-[15px] pb-0 md:pr-5 md:pl-20 md:pt-7 border-b border-[#ebebeb] flex md:block flex-col items-center sticky top-0 bg-white">
       <div className="flex items-center justify-between w-full">
@@ -31,11 +41,16 @@ const SearchResultHeader = () => {
         {menu.map((menu, index) => (
           <span
             key={index}
-            onClick={clickHandler(menu)}
-            className={`flex items-center p-3 text-[#5f6368] cursor-pointer relative `}
+            className={`flex items-center p-3 text-[#5f6368] cursor-pointer relative ${
+              selectedMenu === menu.name ? "text-[#1a73e8]" : ""
+            }`}
+            onClick={() => clickHandler(menu)}
           >
             <span className="hidden md:block mr-2">{menu.icon}</span>
             <span className="text-sm">{menu.name}</span>
+            {selectedMenu === menu.name && (
+              <span className="h-[3px] w-[calc(100%-20px)] absolute bg-[#1a73e8] bottom-0 left-[10px]" />
+            )}
           </span>
         ))}
       </div>
