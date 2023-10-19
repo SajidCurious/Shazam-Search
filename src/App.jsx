8af503c7d50c89c47ed179a6d5236fb4bacbf76c
Mohammed <sajidcurious9@gmail.com>
2023-10-19 13:38:56 +0530

    changed path

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Home />} />
-        <Route path="/" element={<SearchResult />} />
+        <Route path="/:query/:startIndex" element={<SearchResult />} />
       </Routes>
     </BrowserRouter>
   );
