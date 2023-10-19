49f559152435a13a44ce57802865496319d704d0
Mohammed <sajidcurious9@gmail.com>
2023-10-19 20:43:19 +0530

    adding searchResultHeader

--- src/components/SearchResultHeader.jsx
+++ src/components/SearchResultHeader.jsx
@@ -1,14 +1,34 @@
 import { Link } from "react-router-dom";
 import { useContext, useState, useEffect } from "react";
 
-import Logo from "../assets/google-logo.png";
+import Logo from "../assets/s-logos_transparent.png";
 import SearchInput from "./SearchInput";
 import ProfileIcon from "./ProfileIcon";
 import { Context } from "../utils/ContextApi";
 import { menu } from "../utils/Constants";
 
 const SearchResultHeader = () => {
-    return <div>Search result header</div>;
+  return (
+    <div className="p-[15px] pb-0 md:pr-5 md:pl-20 md:pt-7 border-b border-[#ebebeb] flex md:block flex-col items-center sticky top-0 bg-white">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center grow">
+          <Link to="/">
+            <img
+              className="hidden md:block w-[92px] mr-10"
+              src={Logo}
+              alt="Logo"
+            />
+          </Link>
+          <SearchInput from="searchResult" />
+        </div>
+        <div className="hidden md:block">
+          <ProfileIcon />
+        </div>
+      </div>
+
+      <div className="flex ml-[-12px] mt-3"></div>
+    </div>
+  );
 };
 
 export default SearchResultHeader;
