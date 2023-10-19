8895f656a1bcc96db88d0a9fc4d7d90af32f621a
Mohammed <sajidcurious9@gmail.com>
2023-10-19 20:44:06 +0530

    edited

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -14,7 +14,7 @@ const SearchResult = () => {
     <div className="flex flex-col min-h-[100vh]">
       <SearchResultHeader />
       <main className="grow p-[12px] pb-0 md:pr-5 md:pl-20">
-        <div className="flex text-sm text-[#70757a] mb-3"></div>
+        <div className="flex text-sm text-[#70757a] mb-3"> </div>
 
         <Pagination />
       </main>
