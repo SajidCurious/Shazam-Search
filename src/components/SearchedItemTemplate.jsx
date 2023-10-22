ecfc8a16fc9170ce8bed7a25ce1daa8e0b45d86b
Mohammed <sajidcurious9@gmail.com>
2023-10-22 21:42:21 +0530

    completed this component

--- src/components/SearchedItemTemplate.jsx
+++ src/components/SearchedItemTemplate.jsx
@@ -1,4 +1,7 @@
-const SearchedItemTemplate = () => {
+const SearchedItemTemplate = ({ data }) => {
+  function createMarkup(html) {
+    return { __html: html };
+  }
   return (
     <div className="flex flex-col py-3 max-w-[700px]">
       <div
