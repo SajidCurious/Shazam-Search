709ac93afc7e61d2119a00ae34e040303bd9eb13
Mohammed <sajidcurious9@gmail.com>
2023-10-22 14:38:53 +0530

    adding api to the project

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
+
+const params = {
+  key: "AIzaSyCG5c6Jq_NgItRRZBeHfQYVS7qQyjiSdEY",
+  cx: "563b42c4010c24340",
+};
+
 export const fetchDataFromApi = () => {};
